9e0bc31e84f8a05bcb56426bbe5ee9776c5c3da8
REX CONNOR DWIGHT <115325581+rex-connor-dwight@users.noreply.github.com>
2024-01-24 23:46:09 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+
+// Spend a minute memorizing the code
+// Rewrite the line of code
+
+// ReactDOM.render(
+//    [ <h1>Hello, Connor!</h1>,
+//     <ul>
+//         <li>What framework would you love to write on today?</li>
+//         <li>Do you have a prefered code editor?</li>
+//         <li>Would you like your favorite Soptify Playlist?</li>
+//     </ul>
+//     ]
+// , document.getElementById("root"))
+
+
+
+// function Navbar() {
+//     return (
+//         <h1>ToggleBar</h1>
+//     )
+// }
+
+// function Body(){
+//     return (
+//         <p>This is the body of the webpage</p>
+//     )
+// }
+
+// ReactDOM.render(
+//     <div>
+//         <Navbar/>
+//         <Body/>
+//     </div>,
+//     document.getElementById("root")
+// )
+
+// const Navbar = (
+//     <div>
+//         <h1>Soothe</h1>
+//         <ul>
+//             <li>Pricing</li>
+//             <li>About</li>
+//             <li>Contact</li>
+//         </ul>
+//     </div>
+// )
+
+// ReactDOM.render(
+//     Navbar,
+//     document.getElementById("root")
+// )
+
+
+// At the end you shoild have gotten a grab of the basic react build.
